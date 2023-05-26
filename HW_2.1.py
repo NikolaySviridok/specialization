154344822e5625fabd2d505f84d72e30b7a59e8c
NikolaySviridok <sviridok@inbox.ru>
2023-05-26 11:31:34 +0700

    main

---
+++ HW_2.1.py
@@ -0,0 +1,12 @@
+# Задание 1
+# Напишите программу, которая получает целое число и возвращает 
+# его шестнадцатеричное строковое представление. Функцию hex 
+# используйте для проверки своего результата.
+def hex_number(number: int, mod: int = 16) -> str:
+    result = ''
+    while number != 0:
+        temp = number % mod if (number % mod) < 10 else chr(number % mod + 87)
+        result = str(temp) + result
+        number //= mod
+    result = "0x" + result
+    return result
