96395e3e417f4dbdc1e9fcc576d1c50aa44a3d66
NikolaySviridok <sviridok@inbox.ru>
2023-05-27 12:14:59 +0700

    main

--- HW_4.3.py
+++ HW_4.3.py
@@ -1,20 +1,4 @@
 # Задание 3
-# Создайте словарь со списком вещей для похода в качестве ключа 
-# и их массой в качестве значения. Определите какие вещи влезут 
-# в рюкзак передав его максимальную грузоподъёмность. Достаточно 
-# вернуть один допустимый вариант.
-# *Верните все возможные варианты комплектации рюкзака.
-
-def backpack(shop: dict[str:int], size: int) -> list[list[str]]:
-    pcs, weight = zip(*sorted(shop.items(), key=lambda x: x[1], reverse=True))
-    result, temp, temp_w = [], [], 0
-    for index, w in enumerate(weight, 0):
-        temp_w += w
-        temp.append((pcs[index]))
-        for index_n, wn in enumerate(weight[index:], index):
-            if wn + temp_w <= size:
-                temp_w += wn
-                temp.append(pcs[index_n])
-        result.append(temp)
-        temp, temp_w = [], 0
-    return result
+# Возьмите задачу о банкомате из семинара 2. Разбейте её на 
+# отдельные операции — функции. Дополнительно сохраняйте все 
+# операции поступления и снятия средств в список.
