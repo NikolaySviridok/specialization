d7e0c58115143b38ec26498a93b34b4bae61c1fb
NikolaySviridok <sviridok@inbox.ru>
2023-05-27 12:09:32 +0700

    git push

--- HW_4.1.py
+++ HW_4.1.py
@@ -1,7 +1,46 @@
 # Задание 1
-# Дан список повторяющихся элементов. Вернуть список с 
-# дублирующимися элементами. В результирующем списке не 
-# должно быть дубликатов.
+# Напишите функцию для транспорирования матрицы.
 
-def only_double(my_list: list) -> list:
-    return list(set(filter(lambda x: my_list.count(x) > 1, my_list)))
+import random
+
+class Matrix:
+    def __init__(self, rows: int, columns: int):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = Matrix.create_matrix(rows, columns)
+
+    @staticmethod
+    def create_matrix(count_rows: int, count_columns: int) -> list[list]:
+        new_matrix = []
+        for _ in range(count_rows):
+            row = []
+            for _ in range(count_columns):
+                row.append(random.randint(0, 9))
+            new_matrix.append(row)
+        return new_matrix
+
+    def transposition(self):
+        for column in range(self.columns):
+            new_row = []
+            for row in range(self.rows):
+                new_row.append(self.matrix[row][column])
+            self.matrix.append(new_row)
+        for _ in range(self.rows):
+            self.matrix.pop(0)
+
+    def __str__(self):
+        output = ''
+        for row in self.matrix:
+            for item in row:
+                output += f'{item:^3}'
+            output += '\n'
+        return output
+
+
+rows, columns = map(int, input('Введите количество строк и столбцов через пробел: ').split())
+
+matrix = Matrix(rows, columns)
+
+print(matrix)
+matrix.transposition()
+print(matrix)
--- HW_4.2.py
+++ HW_4.2.py
@@ -1,12 +1,19 @@
 # Задание 2
-# В большой текстовой строке подсчитать количество встречаемых 
-# слов и вернуть 10 самых частых. Не учитывать знаки препинания 
-# и регистр символов. За основу возьмите любую статью из 
-# википедии или из документации к языку.
+# Напишите функцию принимающую на вход только ключевые 
+# параметры и возвращающую словарь, где ключ — значение 
+# переданного аргумента, а значение — имя аргумента. Если 
+# ключ не хешируем, используйте его строковое представление.
 
-def ten_popular(text: str) -> list[str]:
-    delete = ".,!?;:-[]{}()="
-    for i in delete:
-        text = text.replace(i, "")
-    text = text.lower()
-    return sorted(set(text.split()), key=lambda x: text.count(x))[-10:]
+def kwargs_to_dict(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        try:
+            result[value] = key
+        except:
+            result[str(value)] = key
+    return result
+
+
+print(kwargs_to_dict(name='Андрей', age=45,
+                     has_work=True, commands=['ККН', 'LM', 'DP'],
+                     growth=1.87, nicks={'STONE', '17th'}))
