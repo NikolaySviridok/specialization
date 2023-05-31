c74cfa80a7cf778e3a75ac1ac2b2b70386c2b841
NikolaySviridok <sviridok@inbox.ru>
2023-05-31 08:09:50 +0700

    main

---
+++ HW_5.2.py
@@ -0,0 +1,12 @@
+# Создайте функцию генератор чисел Фибоначчи.
+
+def fibona44i(limit: int):
+    fibo = [0, 1]
+    while limit > 0:
+        yield fibo[-1]
+        fibo.append(fibo[-1] + fibo[-2])
+        limit -= 1
+
+
+for number in fibona44i(10):
+    print(number)
